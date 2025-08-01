bb816b52052e9239a2f0888c6653d77acce5ebd2
M.A. <221872232+Nyxin-PHP@users.noreply.github.com>
2025-08-01 09:54:44 +0200

    Add files via upload

---
+++ src/Servers/Dojo.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Servers;
+
+class Dojo {
+
+  public function handle(string $password): void {
+    // use {$password} as instance of class Nyxin
+    // or {$word} if you choose dictionary attack
+  }
+}
